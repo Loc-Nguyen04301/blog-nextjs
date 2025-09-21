871a21f82d4a7c7960c48181a177d15fb4639f6b
LocNguyen <locng@fanxipan.net>
2025-09-21 17:47:46 +0700

    add auth resouce

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,9 +6,11 @@ import { CategoryModule } from './category/category.module';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from './common/exception-filters/http-exception.filter';
 import { VideoModule } from './video/video.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule, BlogModule, CategoryModule, VideoModule],
+  imports: [PrismaModule, BlogModule, CategoryModule, VideoModule, AuthModule, UserModule],
   providers: [
     {
       provide: APP_FILTER,
