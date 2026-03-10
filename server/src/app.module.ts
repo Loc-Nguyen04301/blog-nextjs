96ccc3bb357bf835f3a97fb7def9ee4e18f0c231
LocNguyen <nguyengialoc7@gmail.com>
2026-03-10 22:26:56 +0700

    recover code

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,27 +1,33 @@
-import { Module } from '@nestjs/common';
-import { CacheModule } from '@nestjs/cache-manager';
+import { Module } from "@nestjs/common";
+import { CacheModule } from "@nestjs/cache-manager";
 
-import { BlogModule } from './blog/blog.module';
-import { PrismaModule } from './prisma/prisma.module';
-import { CategoryModule } from './category/category.module';
-import { APP_FILTER } from '@nestjs/core';
-import { HttpExceptionFilter } from './common/exception-filters/http-exception.filter';
-import { VideoModule } from './video/video.module';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { LogModule } from './log/log.module';
-import * as redisStore from 'cache-manager-ioredis-yet';
+import { BlogModule } from "./blog/blog.module";
+import { PrismaModule } from "./prisma/prisma.module";
+import { CategoryModule } from "./category/category.module";
+import { APP_FILTER } from "@nestjs/core";
+import { HttpExceptionFilter } from "./common/exception-filters/http-exception.filter";
+import { VideoModule } from "./video/video.module";
+import { AuthModule } from "./auth/auth.module";
+import { UserModule } from "./user/user.module";
+import { LogModule } from "./log/log.module";
+import * as redisStore from "cache-manager-ioredis-yet";
 
 @Module({
-  imports: [PrismaModule, BlogModule, CategoryModule, VideoModule, AuthModule, UserModule,
+  imports: [
+    PrismaModule,
+    BlogModule,
+    CategoryModule,
+    VideoModule,
+    AuthModule,
+    UserModule,
     CacheModule.registerAsync({
       isGlobal: true,
       useFactory: () => ({
         store: redisStore,
-        host: 'localhost',
+        host: "localhost",
         port: 6379,
         ttl: 60 * 1000, // 60s
-      })
+      }),
     }),
     LogModule,
   ],
@@ -32,4 +38,4 @@ import * as redisStore from 'cache-manager-ioredis-yet';
     },
   ],
 })
-export class AppModule { }
+export class AppModule {}
